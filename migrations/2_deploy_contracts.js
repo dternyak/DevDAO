ac12f77d29df3fe761913dea0240741cb40c62cc
Daniel Ternyak <dternyak@gmail.com>
2017-10-29 22:13:09 -0700

    cleanup/get truffle migrate working

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
-var DevDAO = artifacts.require('./DevDAO.sol');
+var ConvertLib = artifacts.require("./ConvertLib.sol");
+var MetaCoin = artifacts.require("./MetaCoin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(DevDAO);
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
 };

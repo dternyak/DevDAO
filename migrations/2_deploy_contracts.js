5b0e5e7ba663d32dea55836a4df873967f79514e
Daniel Ternyak <dternyak@gmail.com>
2017-10-29 23:06:26 -0700

    run migration"

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+var DevDao = artifacts.require("./DevDao.sol");
+module.exports = function (deployer) {
+  deployer.deploy(DevDao, ['0x540d506419bca13cb0c7fc17ddcf2205350a837c',
+  '0x966bd3c5d57b15d2cd893b41a92f2712bf421e34',
+  '0x75d157e9ce14f48efc67253f37b577c703aa63ec',
+  '0xfd15ac22a01cc5667b9fe7752e25af2f4c68c0bf',
+  '0xceac815bc46645357d730b0f53e1291b8e8685fa',
+  '0x3cb0434d6d6b60ccbe900c55ea37d06a1b649f95',
+  '0x3c4b8c6ace4685a2ed6709e421c92c18a5afdce1']);
 };
+
